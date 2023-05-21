6801553f0619a474cdc943a7499194ccded38527
RohitJ1401 <rohitjadhav1401@gmail.com>
2023-05-21 11:36:02 +0530

    Added ShowToastEvent

--- force-app/main/default/lwc/imperativeCallDemo/imperativeCallDemo.js
+++ force-app/main/default/lwc/imperativeCallDemo/imperativeCallDemo.js
@@ -1,5 +1,6 @@
 import { LightningElement, track } from 'lwc';
 import getAccountList from '@salesforce/apex/AccountService.getAccountList';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class ImperativeCallDemo extends LightningElement {
 
@@ -15,12 +16,24 @@ onclickHandler(event){
 
     getAccountList({totalAccounts: this.noOfAccounts})
       .then(result=>{
-        this.accounts = result;
-        this.err = undefined;
+            this.accounts = result;
+              const eventConst = new ShowToastEvent({
+                title : 'Accounts Loaded',
+                message : this.noOfAccounts + ' Accounts are loaded from the server',
+                variant : 'Success',
+              })
+              this.dispatchEvent(eventConst);
 
-      }).catch(error=>{
-        this.accounts = undefined;
-        this.err = error;
+
+            }).catch(error=>{
+              console.error('Error in getting the accounts', error.body.message);
+
+              const constEvent = new ShowToastEvent({
+                title : 'Accounts Failed to Load',
+                message : 'Error Occured',
+                varient : 'Error',
+              })
+              this.dispatchEvent(eventConst);
 
       })
 
